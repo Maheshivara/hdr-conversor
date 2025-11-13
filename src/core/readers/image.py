3c13ffcf84345ca813c8ff5d3177bf66b86c1dfd
Maheshivara <gabrimath@gmail.com>
2025-11-13 15:22:59 -0300

    refactor: :recycle: replace wand by pillow

--- src/core/readers/image.py
+++ src/core/readers/image.py
@@ -3,6 +3,7 @@ import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 import cv2 as cv
 
+
 class ImageReader:
     def read_image(self, filepath: str):
         return cv.imread(filepath, cv.IMREAD_UNCHANGED)
